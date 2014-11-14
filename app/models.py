9750166809dc4e921ac75ca243a206fe78dd6a52
Jake Meyer <jacob.robert.meyer@gmail.com>
2014-11-13 21:54:30 -0500

    roles

--- app/models.py
+++ app/models.py
@@ -9,15 +9,40 @@ from flask import current_app
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
+class Permission:
+	FOLLOW = 0x01
+	COMMENT = 0x02
+	WRITE_ARTICLES = 0x04
+	MODERATE_COMMENTS = 0x08
+	ADMINISTER = 0x80
+
 class Role(db.Model):
 	__tablename__ = 'roles'
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(64), unique=True)
+	default = db.Column(db.Boolean, default=False, index=True)
+	permissions = db.Column(db.Integer)
 
 	def __repr__(self):
 		return '<Role %r>' % self.name
 
-	users = db.relationship('User', backref='role')
+	@staticmethod
+	def insert_roles():
+		roles = {
+			'User' : (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES, True),
+			'Moderator' : (Permission.FOLLOW | Permission.COMMENT | Permission.WRITE_ARTICLES | Permission.MODERATE_COMMENTS, False),
+			'Administrator' : (0xff, False)
+		}
+		for r in roles:
+			role = Role.query.filter_by(name=r).first()
+			if role is None:
+				role = Role(name=r)
+			role.permissions = roles[r][0]
+			role.default = roles[r][1]
+			db.sessions.add(role)
+		db.session.commit()
+
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
 class User(UserMixin, db.Model):
 	__tablename__ = 'users'
