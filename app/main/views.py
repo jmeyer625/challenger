dfe09f52eb9eb381e489b779db2cf78ba51a4242
Jake Meyer <jacob.robert.meyer@gmail.com>
2014-11-15 18:35:01 -0500

    context processor

--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,23 @@ from datetime import datetime
 from flask import render_template, session, redirect, url_for
 from . import main
 from .. import db
-from ..models import User
+from ..models import User, Permission
+from ..decorators import admin_required, permission_required
+from flask.ext.login import login_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
 	return render_template('index.html', name = session.get('name'), known=session.get('known', False), \
 			current_time = datetime.utcnow())
+
+@main.route('/admin')
+@login_required
+@admin_required
+def for_admins_only():
+	return 'For administrators'
+
+@main.route('/moderator')
+@login_required
+@permission_required(Permission.MODERATE_COMMENTS)
+def for_moderators_only():
+	return 'For comment moderators'
